47841646675152bd45e47595cdebd8fd09ed5c80
dervin <ddervin2003@gmail.com>
2025-07-08 13:40:53 +0530

    loops and conditional statements

--- index.js
+++ index.js
@@ -1,80 +1,238 @@
-//Types of variables
-let a=1;
-let b='1';
-if(a==b){
-    console.log("true");
+// //Types of variables
+// let a=1;
+// let b='1';
+// if(a==b){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// if(a===b){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// let x=3;
+// let y=3;
+// if(x==y){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+// if(x===y){
+//     console.log("true");
+// }
+// else{
+//     console.log("false");
+// }
+
+
+// var c=1;
+// if (c==1){
+//     var c=2;
+// }
+// console.log(a)
+
+// let d=1;
+// if(d==1){
+//     let d=2;
+// }
+// console.log(b)
+
+// const e=2;   // cannot change the value
+
+// // Data types
+// console.log(typeof(a));
+// console.log(typeof(b));
+
+// let f;
+
+// let g=false;
+
+// console.log(typeof(f));
+// console.log(typeof(g));
+
+// // Ternary operators
+
+// g=(g==false)?true:false;
+// console.log(g);
+
+// //Operators
+// let l=10;
+// let m=20;
+
+// console.log("add : "+(l+m));
+// console.log("subtract : "+(l-m));
+// console.log("Multiply : "+l*m);
+// console.log("Divide : "+l/m);
+// console.log("Modulus : "+l%m);
+// console.log("Exponential : "+l**m);
+
+// console.log("null==undefined");
+// console.log("null===undefined");
+// console.log('5'-2);
+// console.log(typeof Symbol("id"));
+
+
+// for(let i=0;i<5;++i){
+//     console.log(i);
+// }
+
+
+// const person = ["Dervin0","Dervin1","Dervin2"]
+
+// for(let x in person){
+//     console.log(x);
+// }
+
+// for(let x of person){
+//     console.log(x)
+// }
+
+// const mark=70;
+// if (mark>=90 & mark<=100){
+//     console.log("Grade 'A'");
+// }
+// else if (mark>=70){
+//     console.log("Grade 'B'");
+// }
+// else if (mark>=50){
+//     console.log("Grade 'C'");
+// }
+// else if(mark>100){
+//     console.log("The mark should be 100 or below")
+// }
+// else{
+//     console.log("Fail");
+// }
+
+// for(let i=1 ; i<=5 ;i++){
+//     console.log(i);
+// }
+
+// console.log("Example 1")
+// for(let i=1 ; i<=10 ;i++){
+//     console.log(i++);
+//     if (i===5){
+//         break;
+//     }
+// }
+
+// console.log("Example 2")
+// for(let i=1 ; i<=10 ;i++){
+//     if (i===5){
+//         break;
+//     }
+//     console.log(i++);
+// }
+
+// console.log("Example 3")
+// for(let i=1 ; i<=10 ;++i){
+//     console.log(i++);
+//     if (i===5){
+//         break;
+//     }
+// }
+
+// console.log("Example 4")
+// for(let i=1 ; i<=10 ;++i){
+//     if (i===5){
+//         break;
+//     }
+//     console.log(i++);
+// }
+
+// console.log("Example 5")
+// for(let i=1 ; i<=10 ;i++){
+//     console.log(++i);
+//     if (i===5){
+//         break;
+//     }
+// }
+
+// console.log("Example 6")
+// for(let i=1 ; i<=10 ;i++){
+//     if (i===5){
+//         break;
+//     }
+//     console.log(++i);
+// }
+
+// console.log("Example 7")
+// for(let i=1 ; i<=10 ;++i){
+//     console.log(++i);
+//     if (i===5){
+//         break;
+//     }
+// }
+
+// console.log("Example 8")
+// for(let i=1 ; i<=10 ;++i){
+//     if (i===5){
+//         break;
+//     }
+//     console.log(++i);
+// }
+
+
+// let dayNumber=6;
+// let dayName;
+// switch(dayNumber){
+//     case 1:
+//         dayName="Monday";
+//         break;
+//     case 2:
+//         dayName="Tuesday";
+//         break;
+//     case 3:
+//         dayName="Wednesday";
+//         break;
+//     case 4:
+//         dayName="Thursday";
+//         break;
+//     case 5:
+//         dayName="Friday";
+//         break;
+//     case 6:
+//     case 7:
+//         dayName="Week End"
+//         break;
+//     default:
+//         dayName="Invalid Day"
+// }
+
+// console.log(dayName);
+
+
+// let word="NOTHING"
+// let i=0;
+
+// for (x of word){
+//     // console.log(x);
+//     i++
+// }
+// console.log(i)
+
+// const fruits = ['Apple', 'Banana', 'Cherry'];
+// let txt = '';
+// for (const x in fruits) {
+//   txt += x;
+// }
+
+// console.log(txt)
+
+it = 0 ;
+while(it < 5){
+    console.log(it);
+    it++;
 }
-else{
-    console.log("false");
-}
 
-if(a===b){
-    console.log("true");
-}
-else{
-    console.log("false");
-}
-
-let x=3;
-let y=3;
-if(x==y){
-    console.log("true");
-}
-else{
-    console.log("false");
-}
-
-if(x===y){
-    console.log("true");
-}
-else{
-    console.log("false");
-}
-
-
-var c=1;
-if (c==1){
-    var c=2;
-}
-console.log(a)
-
-let d=1;
-if(d==1){
-    let d=2;
-}
-console.log(b)
-
-const e=2;   // cannot change the value
-
-// Data types
-console.log(typeof(a));
-console.log(typeof(b));
-
-let f;
-
-let g=false;
-
-console.log(typeof(f));
-console.log(typeof(g));
-
-// Ternary operators
-
-g=(g==false)?true:false;
-console.log(g);
-
-//Operators
-let l=10;
-let m=20;
-
-console.log("add : "+(l+m));
-console.log("subtract : "+(l-m));
-console.log("Multiply : "+l*m);
-console.log("Divide : "+l/m);
-console.log("Modulus : "+l%m);
-console.log("Exponential : "+l**m);
-
-console.log("null==undefined");
-console.log("null===undefined");
-console.log('5'-2);
-
-
+do{
+    console.log(it);
+    it++;
+}while(it<10);
